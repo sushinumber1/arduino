444da22244693e662faa7dfa2fdebec911bf26f2
sushinumber1 <sushinumber1@gmail.com>
2012-06-25 09:05:03 +0200

    add files

---
+++ stepper/stepper4_simple.ino
@@ -0,0 +1,142 @@
+/* Stepper Copal
+ * -------------
+ *
+ * Program to drive a stepper motor coming from a 5'25 disk drive
+ * according to the documentation I found, this stepper: "[...] motor 
+ * made by Copal Electronics, with 1.8 degrees per step and 96 ohms 
+ * per winding, with center taps brought out to separate leads [...]"
+ * [http://www.cs.uiowa.edu/~jones/step/example.html]
+ *
+ * It is a unipolar stepper motor with 5 wires:
+ * 
+ * - red: power connector, I have it at 5V and works fine
+ * - orange and black: coil 1
+ * - brown and yellow: coil 2
+ *
+ * (cleft) 2005 DojoDave for K3
+ * http://www.0j0.org | http://arduino.berlios.de
+ *
+ * @author: David Cuartielles
+ * @date: 20 Oct. 2005
+ */
+
+int motorPin1 = 8;
+int motorPin2 = 9;
+int motorPin3 = 10;
+int motorPin4 = 11;
+int delayTime = 10;
+//int init;
+
+void setup() {
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+  Serial.begin(9600);
+//  init=4;
+}
+int foo =4; //motor phase init outside the loop for initialisation
+void loop() {
+int stop_phase;
+
+stop_phase= clockwise(3,foo);
+Serial.println("stop phase : ");
+Serial.println(foo);
+//Serial.println("foo : ");
+//Serial.println(foo);
+//foo++;
+foo=stop_phase;
+  delay(1000);
+  
+
+
+}
+
+int clockwise(int n, int stop_phase)
+{
+  int i=0;
+
+while(i<n)
+  {
+  if((i!=n-1)&&(stop_phase=4)){
+  phase1();
+  i++;
+    if((i!=n-1)&&(stop_phase=1)){
+    phase2();
+    i++;
+       if((i!=n-1)&&(stop_phase=2)){
+       phase3();
+       i++;
+              if((i!=n-1)&&(stop_phase=3)){
+               phase4();
+               i++;
+               }
+             else{return 4;stop_phase=4;}
+       }
+       else{return 3;stop_phase=3;}
+    }
+    else{return 2;stop_phase=2;}      
+  }
+  else{return 1;stop_phase=1;}
+ }
+ 
+}
+
+
+void phase1()
+{
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+}
+void phase2()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+}
+void phase3()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+}
+void phase4()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  delay(delayTime);
+}
+
+void sens_anti_horraire()
+{
+    //sens inverse des aiguille d'une montre
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  delay(delayTime);
+    digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+    digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+    digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+}
